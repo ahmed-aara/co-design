8881cc805fb0c677e6a85ae33baf85cea89f30d1
Ahmed Abd Rb Alrasol <ahmedabdrbalrasol2015.aara@gmail.com>
2022-12-31 00:47:22 +0200

    New Update

--- src/app/page/landing-page/landing-page.component.ts
+++ src/app/page/landing-page/landing-page.component.ts
@@ -40,6 +40,21 @@ export class LandingPageComponent implements OnInit {
     },
   ]
 
+  aboutArr: any = [
+    {
+      title: 'Office Design',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also.'
+    },
+    {
+      title: 'Residential Design',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also.'
+    },
+    {
+      title: 'Commercial Design',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also.'
+    },
+  ]
+
   placeArr: any = [
     { name: 'Our Office', value: 'office' },
     { name: 'Mandi Man', value: 'mandi-man' },
