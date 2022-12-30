46800306e360abe2d67daf6047de92878d204c21
Ahmed Abd Rb Alrasol <ahmedabdrbalrasol2015.aara@gmail.com>
2022-12-30 16:49:47 +0200

    New Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './page/about/about.component';
 import { ContactComponent } from './page/contact/contact.component';
+import { LandingPageComponent } from './page/landing-page/landing-page.component';
 import { OurServiceComponent } from './page/our-service/our-service.component';
 import { OurWorkComponent } from './page/our-work/our-work.component';
 import { TestComponent } from './page/test/test.component';
 
 const routes: Routes = [
+  { path: '', component: LandingPageComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'work', component: OurWorkComponent },
--- src/app/page/landing-page/landing-page.component.ts
+++ src/app/page/landing-page/landing-page.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class LandingPageComponent {
 
+  serviceArr: any = [
+    {
+      icon: 'assets/img/vector/paint.svg',
+      title: 'Interior Expertise',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when.'
+    },
+    {
+      icon: 'assets/img/vector/awward.svg',
+      title: 'Best Designs',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when.'
+    },
+    {
+      icon: 'assets/img/vector/wallet.svg',
+      title: 'REASONABLE PRICE',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when.'
+    },
+    {
+      icon: 'assets/img/vector/chat.svg',
+      title: 'Free Consultation',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when.'
+    },
+    {
+      icon: 'assets/img/vector/call.svg',
+      title: '24 / 7 Support',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when.'
+    },
+    {
+      icon: 'assets/img/vector/recycle.svg',
+      title: 'Guaranteed Works',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when.'
+    },
+  ]
+
 }
